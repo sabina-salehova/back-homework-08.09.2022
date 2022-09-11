e08df17ee1ba5565812e80a3920f251a9652d16d
sabina-salehova <sabinaas@code.edu.az>
2022-09-12 03:24:30 +0400

    added index page

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace homework_08._09._2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
